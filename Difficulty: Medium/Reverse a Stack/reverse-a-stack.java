b9f47f13553170e944dd57288fa9b8d633d9986a
Roshini S <roshforstudy@gmail.com>
2026-07-27 21:30:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse a Stack/reverse-a-stack.java
+++ Difficulty: Medium/Reverse a Stack/reverse-a-stack.java
@@ -1,35 +1,33 @@
 class Solution {
     public static void reverseStack(Stack<Integer> st) {
         // code here
-          // Base case
-        if (st.isEmpty()) {
-            return;
+        /* simple analogy 8 4 1 2 , 8 is top
+        how to reverse simple like remove the top element insert at bottom
+        remove the top insert at bottom
+        
+        
+        so like this we are going to call;*/
+        if(st.isEmpty())
+        {
+            return ;//as the given method is void;
         }
-
-        // Remove the top element
-        int x = st.pop();
-
-        // Reverse the remaining stack
-        reverseStack(st);
-
-        // Insert removed element at the bottom
-        insertAtBottom(st, x);
+        
+        int top=st.pop();//remove the top element
+        reverseStack(st);//recursive call
+        insertAtBottom(st,top);//insert at bottom
     }
-
-    private static void insertAtBottom(Stack<Integer> st, int x) {
-
-        if (st.isEmpty()) {
-            st.push(x);
-            return;
-        }
-
-        int temp = st.pop();
-
-        insertAtBottom(st, x);
-
-        st.push(temp);
-    }
-      
-
     
+    public static Stack<Integer> insertAtBottom(Stack<Integer>st,int x)
+    {
+        if(st.isEmpty())
+        {
+            st.push(x);
+            return st;
+        }
+        
+        int top=st.pop();
+        insertAtBottom(st,x);
+        st.push(top);
+        return st;
+    }
 }
